a754ecc168ef3447f46b007628fbd6b97f1d1f27
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-02-04 05:06:39 +0300

    REDME edit && logo && layout settings

--- src/components/navigation/TopNavBar.tsx
+++ src/components/navigation/TopNavBar.tsx
@@ -47,8 +47,9 @@ const TopNavBar = () => {
             </button>
             <Link to='/'>
               <div className='flex ml-2 md:mr-24'>
-                <span className='self-center text-2xl font-roboto  font-bold whitespace-nowrap dark:text-white'>
-                  Logo.
+                <span className='flex items-center gap-2 self-center text-2xl font-roboto  font-bold whitespace-nowrap dark:text-white'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
+                 Portal
                 </span>
               </div>
             </Link>
--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -11,7 +11,7 @@ const PageLayout: React.FunctionComponent<LayoutProps> = (props: LayoutProps) =>
       <div>{props.SideNavBar}</div>
       <div className='h-screen  w-full bg-gray-100 dark:bg-gray-900 flex flex-col gap-20 '>
         <div className=''>{props.TopNavBar}</div>
-        <div className=' px-9 md:px-2 sm:ml-64  flex flex-col-reverse gap-5 md:flex-row-reverse md:items-center justify-between '>
+        <div className=' px-9 md:px-2 sm:ml-64  flex flex-col-reverse gap-5 md:flex-row-reverse md:items-center justify-end '>
           <div>{props.ImportButton}</div>
           <div className='w-full md:w-sixty '>{props.SearchInput}</div>
         </div>
