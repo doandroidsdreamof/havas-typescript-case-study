b51117f4481dc5aff4edad488fce8f0953d9c672
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-02-04 08:44:24 +0300

    request interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -9,7 +9,7 @@ export interface LayoutProps {
   CommentsCard?: React.ReactNode;
   UsersCard?: React.ReactNode;
   PhotosCard?: React.ReactNode;
-  RemovePosts?:  React.ReactNode;
+  RemovePosts?: React.ReactNode;
 }
 
 //* Dark mode context-api //
@@ -26,4 +26,73 @@ export interface Posts {
   id?: number;
   title?: string;
   body?: string;
+}
+
+//* Comments request //
+
+export type CommnetsRoot = Comments[];
+
+export interface Comments {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+//* Photos request //
+
+export type PhotosRoot = Photos[];
+
+export interface Photos {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+//* Todos request //
+
+export type TodosRoot = Todos[];
+
+export interface Todos {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+//* Users request //
+
+export type UsersRoot = Users[];
+
+export interface Users {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
