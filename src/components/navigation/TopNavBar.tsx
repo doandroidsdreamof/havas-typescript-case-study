a3e06a2fb3e0067388c2a1ea9971506c3565b275
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-02-04 04:52:32 +0300

    ReadMe template init && bgColor

--- src/components/navigation/TopNavBar.tsx
+++ src/components/navigation/TopNavBar.tsx
@@ -26,7 +26,7 @@ const TopNavBar = () => {
   };
 
   return (
-    <nav className='fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
+    <nav className='fixed top-0 z-50 w-full shadow-sm bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
       <div className='px-3 py-3 lg:px-5 lg:pl-3'>
         <div className='flex items-center justify-between'>
           <div className='flex items-center justify-start'>
--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -9,7 +9,7 @@ const PageLayout: React.FunctionComponent<LayoutProps> = (props: LayoutProps) =>
   return (
     <>
       <div>{props.SideNavBar}</div>
-      <div className='h-screen  w-full dark:bg-gray-900 flex flex-col gap-20 '>
+      <div className='h-screen  w-full bg-gray-100 dark:bg-gray-900 flex flex-col gap-20 '>
         <div className=''>{props.TopNavBar}</div>
         <div className=' px-9 md:px-2 sm:ml-64  flex flex-col-reverse gap-5 md:flex-row-reverse md:items-center justify-between '>
           <div>{props.ImportButton}</div>
