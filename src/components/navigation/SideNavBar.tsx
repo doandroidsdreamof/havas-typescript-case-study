44ca9acd60281dddbbd76218eb3b1f2800a9fda0
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-02-04 05:17:09 +0300

    darkMode transtiion effect

--- src/components/navigation/SideNavBar.tsx
+++ src/components/navigation/SideNavBar.tsx
@@ -48,8 +48,8 @@ const SideNavBar = () => {
       id='logo-sidebar'
       className={
         navbarState
-          ? 'fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700'
-          : 'fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700'
+          ? 'notApplyDelay fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700'
+          : 'notApplyDelay fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700'
       }
       aria-label='Sidebar'
     >
